e0cd5a510e7ff43e494b61cae5f551293e6ab9bd
Johan Euphrosine <proppy@google.com>
2014-10-17 16:24:11 -0700

    hooks: reorganize and document

--- hooks.js
+++ hooks.js
@@ -3,15 +3,21 @@ var request = require('request');
 var hooks = require('hooks');
 var url = require('url');
 
-function uri(t, path) {
-  return url.format({
-    protocol: 'http',
-    hostname: t.host,
-    port: t.port,
-    pathname: path
+// DELETE /todos
+// after: check that there is no more completed todos.
+hooks.after('Todos > Todos Collection > Archive completed Todos', function(t, done) {
+  request.get({
+    uri: uri(t, 'todos')
+  }, function(err, res, body) {
+    JSON.parse(body).forEach(function(todo) {
+      console.assert(!todo.complete);
+    });
+    return done();
   });
-}
+});
 
+// GET /todos/:id
+// before: create a new todo.
 hooks.before('Todos > Todo > Get a Todo', function(t, done) {
   request.post({
     uri: uri(t, 'todos'),
@@ -22,6 +28,9 @@ hooks.before('Todos > Todo > Get a Todo', function(t, done) {
   });
 });
 
+// DELETE /todos/:id
+// before: create a new todo.
+// after: check that the todo is gone.
 hooks.before('Todos > Todo > Delete a Todo', function(t, done) {
   request.post({
     uri: uri(t, 'todos'),
@@ -41,13 +50,11 @@ hooks.after('Todos > Todo > Delete a Todo', function(t, done) {
   });
 });
 
-hooks.after('Todos > Todos Collection > Archive done Todos', function(t, done) {
-  request.get({
-    uri: uri(t, 'todos')
-  }, function(err, res, body) {
-    JSON.parse(body).forEach(function(todo) {
-      console.assert(!todo.complete);
-    });
-    return done();
+function uri(t, path) {
+  return url.format({
+    protocol: 'http',
+    hostname: t.host,
+    port: t.port,
+    pathname: path
   });
-});
+}
